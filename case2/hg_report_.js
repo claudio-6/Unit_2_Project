d8ce4186eaf3c70414afbc25223fdc498b97205d
claudio <crios235@west-mec.org>
2020-02-06 16:36:58 -0700

    this is case 2

--- case2/hg_report_.js
+++ case2/hg_report_.js
@@ -11,3 +11,31 @@
    Filename: hg_report.js
 	
 */
+//displays the information on html
+var gameReport = "<h1>" + itemTitle + "</h1> <h2> By:" + itemManufacturer + "</h2> <img src='hg_" + itemID + ".png' alt='" + itemID + "' id='gameImg'/> <table> <tr><th>Product ID</th><td>" + itemID + "</td></tr> <tr><th>List Price</th><td>" + itemPrice + "</td></tr> <tr><th>Platform</th><td>" + itemPlatform + "</td></tr> <tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr> <tr><th>Condition</th><td>" + itemCondition + "</td></tr> <tr><th>Release</th><td>" + itemRelease + "</td></tr> </table>" + itemSummary;
+// display gameReport 
+document.getElementsByTagName("article")[0].innerHTML = gameReport;
+//creating variables
+var ratingsSum = 0;
+var ratingsCount = ratings.length;
+//adding ratings together
+for (var i = 0; i < ratings.length; i++) {
+    ratingsSum += ratings[i];
+}
+//gets the average
+var ratingsAvg = ratingsSum / ratingsCount;
+//Heading 
+var ratingReport = "<h1>Customer Reviews</h1> <h2>" + ratingsAvg + " out of 5 stars (" + ratingsCount + " reviews)</h2>";
+//making the reviews and star ratings
+for (var i = 0; i <= 2; i++) {
+    ratingReport += "<div class='review'>";
+    ratingReport += "<h1>" + ratingTitles[i] + "</h1>";
+    ratingReport += "<table> <tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr>";
+    ratingReport += "<tr><th>Review Date</th><td>" + ratingDates[i] + "</td></tr>";
+    ratingReport += "<tr><th>Rating</th><td>";
+    for (var j = 0; j < ratings[i]; j++) {
+        ratingReport += "<img src='hg_star.png'/>";
+    }
+    ratingReport += "</td></tr></table>" + ratingSummaries[i] + "</div>";
+}
+document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
